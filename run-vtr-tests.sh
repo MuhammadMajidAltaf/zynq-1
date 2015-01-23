800fa4406d45c116b54fe7871a2729cbc9d89d77
Ali Lown <ali@lown.me.uk>
2015-01-23 16:50:28 +0000

    Update run-vtr-tests

--- run-vtr-tests.sh
+++ run-vtr-tests.sh
@@ -3,9 +3,8 @@
 DIR=~/results-`date +%s`
 mkdir $DIR
 
-#Run VTR tests
 cd ~/vtr
-for i in 667 333 222
+for i in 667 333
 do
 	echo "Running test for speed $i"
 
@@ -14,24 +13,24 @@ do
 	`cpufreq-set -c1 -f ${i}MHz`
 
 	echo ">Start PM"
-	TIME=150*667/$i
+  TIME=250*$(( 667/$i ))
 	PM_LOG="$DIR/vtr-pm-$i"
+
 	(~/monit/stats.rb 2 $TIME $PM_LOG)&
 
 	sleep 10
 
 	echo ">Running test"
 	VTR_LOG="$DIR/vtr-out-$i"
-	START=`date +%s`
-	(./run_reg_test.pl vtr_reg_basic > $VTR_LOG)&
+	(START=`date +%s`; 
+	./run_reg_test.pl vtr_reg_basic > $VTR_LOG
+	 END=`date +%s`;
+	 TIME_LOG="$DIR/vtr-time-$i";
+	 echo $START >> $TIME_LOG;
+	 echo $END >> $TIME_LOG;
+        )&
 
 	wait
-	END=`date +%s`
-
-	TIME_LOG="$DIR/vtr-time-$i"
-	echo $START >> $TIME_LOG
-	echo $END >> $TIME_LOG
-	echo $($END - $START) >> $TIME_LOG
 
 	echo ">Cooling down"
 	sleep 30
