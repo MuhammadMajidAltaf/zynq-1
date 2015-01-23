7c675f72fff20565cb27a0706286505120419269
Ali Lown <ali@lown.me.uk>
2015-01-23 16:43:19 +0000

    Update run-x264-tests to get accurate times

--- run-x264-tests.sh
+++ run-x264-tests.sh
@@ -3,9 +3,7 @@
 DIR=~/results-`date +%s`
 mkdir $DIR
 
-#Run VTR tests
-cd ~/vtr
-for i in 667 333 222
+for i in 667 333
 do
 	echo "Running test for speed $i"
 
@@ -14,7 +12,7 @@ do
 	`cpufreq-set -c1 -f ${i}MHz`
 
 	echo ">Start PM"
-	TIME=150*667/$i
+	TIME=80
 	PM_LOG="$DIR/vtr-pm-$i"
 	(~/monit/stats.rb 2 $TIME $PM_LOG)&
 
@@ -22,16 +20,17 @@ do
 
 	echo ">Running test"
 	VTR_LOG="$DIR/vtr-out-$i"
-	START=`date +%s`
 	X264_INST=/root/x264/x264-snapshot-20141218-2245/x264
-	($X264_INST ~/Sintel.y4m -o ~/a.h264 > $VTR_LOG)&
+	(START=`date +%s`; 
+	 $X264_INST ~/Sintel.y4m -o ~/a.h264 > $VTR_LOG;
+	 END=`date +%s`;
+	 TIME_LOG="$DIR/vtr-time-$i";
+	 echo $START >> $TIME_LOG;
+	 echo $END >> $TIME_LOG;
+        )&
 
 	wait
-	END=`date +%s`
 
-	TIME_LOG="$DIR/vtr-time-$i"
-	echo $START >> $TIME_LOG
-	echo $END >> $TIME_LOG
 
 	GMON_FM="$DIR/vtr-gmount.out-$i"
 	cp ~/gmon.out $GMON_FM
