d7512898d8516e07953caaa91b71641362f34a8a
Ali Lown <ali@lown.me.uk>
2015-01-31 22:15:24 +0000

    Add a simd round

--- elf/analysis.rb
+++ elf/analysis.rb
@@ -6,7 +6,29 @@ BRANCH_INSNS = ['bl', 'blx', 'bx', 'cbnz', 'cbz', 'b', 'tbb', 'tbh']
 BRANCH_SUFFIXES = ['eq', 'ne', 'cs', 'cc', 'mi', 'pl', 'vs', 'vc', 'hi', 'ls', 'ge', 'lt', 'gt', 'le']
 BRANCH_IT = ['it', 'itt', 'ite', 'itt', 'itet' ,'itte', 'itee', 'itttt', 'itett', 'ittet', 'iteet', 'ittte' ,'itete' ,'ittee', 'iteee']
 
-ARITH_INSNS = ['adc', 'add', 'adds', 'addw', 'and', 'bic', 'eor', 'mov', 'mvn', 'orn', 'orr', 'rsb', 'sbc', 'sub', 'subs', 'subw', 'asr', 'asl', 'lsl', 'lsr', 'ror', 'rrx', 'mla', 'mls' , 'mul', 'smlal', 'smull', 'umlal', 'umull', 'ssat', 'usat', 'sxtb', 'sxth', 'uxtb', 'uxth', 'sdiv', 'udiv', 'bfc', 'bfi', 'clz', 'movt', 'rbit', 'rev', 'rev16', 'revsh', 'sbfx', 'ubfx']
+SHIFTS = ['lsl', 'lsr', 'asr', 'ror', 'rrx']
+
+DP_INSNS = ['adc', 'add', 'adr', 'and', 'bic', 'cmn', 'cmp', 'eor', 'mov', 'mvn', 'orn', 'orr', 'rsb', 'rsc', 'sbc', 'sub',] #none: 'teq', 'tst'
+SHIFT_INSNS = ['asr', 'lsl', 'lsr', 'ror', 'rrx']
+MUL_INSNS = ['mla', 'mls', 'mul', 'smlabb', 'smlabt', 'smlatb', 'mslatt', 'smlad', 'smlal', 'smlalbb', 'smlalbt', 'smlaltb', 'smlaltt', 'smlald', 'smlawb', 'smlawt', 'smlsd', 'smlsld', 'smmla', 'smlls', 'smmul', 'smuad', 'smulbb', 'smulbt', 'smultb', 'smultt', 'smull', 'smulwb', 'smulwt', 'smusd', 'umaal', 'umlal', 'umull']
+SAT_INSNS = ['ssat', 'ssat16', 'usat', 'usat16', 'qadd', 'qsub', 'qdadd', 'qdsub']
+PACK_INSNS = ['pkh', 'sxtab', 'sxtab16', 'sxtah', 'sxtb', 'sxtb16', 'sxth', 'uxtab', 'uxtab16', 'uxtah', 'uxtb', 'uxtb16', 'uxth']
+PAR_INSNS = ['sadd16', 'qadd16', 'shadd16', 'uadd16', 'uqadd16', 'uhadd16', 'sasx', 'qasx', 'shasx', 'uasx', 'uqasx', 'uhasx', 'ssax', 'qsax', 'shsax', 'usax', 'uqsax', 'uhsax', 'ssub16', 'qsub16', 'shsub16', 'usub16', 'uqsub16', 'uhsub16', 'sadd8', 'qadd8', 'shad8', 'uadd8', 'uqadd8', 'uhadd8', 'ssub8', 'qsub8', 'shsub8', 'usub8', 'uqsub8', 'uhsub8']
+DIV_INSNS = ['sdiv', 'udiv']
+MISC_INSNS = ['bfc', 'bfi', 'clz', 'movt', 'rbit', 'rev', 'rev16', 'revsh', 'sbfx', 'sel', 'ubfx', 'usad8', 'usada8']
+LDST_INSNS = ['ldr', 'str', 'ldrt', 'strt', 'ldrex', 'strex', 'strh', 'strht', 'strexh', 'ldrh', 'ldrht', 'ldrexh', 'ldrsh', 'ldrsht', 'strb', 'strbt', 'strexb', 'ldrb', 'ldrbt', 'ldrexb', 'ldrsb', 'ldrsbt', 'ldrd', 'strd', 'ldrexd', 'strexd']
+LDSTMUL_INSNS = ['ldm', 'ldmia', 'ldmfd', 'ldmda', 'ldmfa', 'ldmdb', 'ldmea', 'ldmib', 'ldmed', 'pop', 'push', 'stm', 'stmia', 'stmea', 'stmda', 'stmed', 'stmdb', 'stmfd', 'stmib', 'stmfa']
+
+SIMD_INSNS = ['vldm', 'vldr', 'vstm', 'vstr', 'vdup', 'vmov', 'vmrs', 'vmsr']
+SIMD_PAR_INSNS = ['vadd', 'vaddhn', 'vaddl', 'vaddw', 'vhadd', 'vhsub', 'vpadal', 'vpadd', 'vpaddl', 'vraddhn', 'vrhadd', 'vrsubhn', 'vqadd', 'vqsub', 'vsub', 'vsubhn', 'vsubl', 'vsubw']
+SIMD_BIT_INSNS = ['vand', 'vbic', 'veor', 'vbif', 'vbit', 'vbsl', 'vmov', 'vmvn', 'vorr', 'vorn']
+SIMD_ADV_INSNS = ['vagce', 'vacgt', 'vacle', 'vaclt', 'vceq', 'vcge', 'vcgt', 'vcle'] #none: 'vtst'
+SIMD_SHIFT_INSNS = ['vqrshl', 'vqrshrn', 'vqrshrun', 'vqshl', 'vqshlu', 'vqshrn', 'vqshrun', 'vrshl', 'vrshr', 'vrsa', 'vrshn', 'vshl', 'vshll', 'vshr', 'vshrn', 'vsli', 'vsra', 'vsri']
+SIMD_MUL_INSNS = ['vmla', 'vmlal', 'vmls', 'vmlsl', 'vmul', 'vmull', 'vfma', 'vfms', 'vqdmlal', 'vqdmlsl', 'vqdmul', 'vqrdmulh', 'vqdmull']
+SIMD_MISC_INSNS = ['vaba', 'vabal', 'vabd', 'vabdl', 'vabs', 'vcvt', 'vcls', 'vclz', 'vcnt', 'vdup', 'vext', 'vmovn', 'vmovl', 'vmax', 'vmin', 'vneg', 'vpmax', 'vpmin', 'vrecpe', 'vrecps', 'vrsqrte', 'vrsqrts', 'vrev16', 'vrev32', 'vrev64', 'vqabs', 'vqmovn', 'vqmovun', 'vqneg', 'vswp', 'vtbl', 'vtbx', 'vtrn', 'vuzp', 'vzip']
+
+ARITH_INSNS = DP_INSNS + SHIFT_INSNS + MUL_INSNS + SAT_INSNS + PAR_INSNS + DIV_INSNS + MISC_INSNS
+SIMD_ARITH_INSNS = SIMD_PAR_INSNS + SIMD_BIT_INSNS + SIMD_SHIFT_INSNS + SIMD_MUL_INSNS + SIMD_ADV_INSNS + SIMD_MISC_INSNS
 
 def strip_line(line)
   return {addr: line[:addr], raw: line[:raw], instr: line[:instr], args: line[:args]}
@@ -158,7 +180,7 @@ bbs_asp.each do |bb|
   puts "order analysing func: #{bb[:func]}@#{bb[:addr]}"
 
   used_regs = {}
-  bb[:code].map! do |line|
+  bb[:code].each do |line|
     line[:deps] = []
     line[:deps_lines] = []
 
@@ -178,8 +200,6 @@ bbs_asp.each do |bb|
         used_regs[arg] = line
       end
     end
-
-    line
   end
 
   puts "parallelizing func: #{bb[:func]}@#{bb[:addr]}"
@@ -205,3 +225,25 @@ bbs_asp.each do |bb|
 end
 
 puts "Verilog Generation concluded"
+
+#################################
+# SIMD analysis
+#################################
+
+puts "Starting SIMD analysis"
+
+bbs.each do |bb|
+  puts "analysing func: #{bb[:func]}@#{bb[:addr]}"
+
+  bb[:has_simd] = false
+
+  bb[:code].each do |line|
+    if SIMD_ARITH_INSNS.include? line[:instr] then
+      bb[:has_simd] = true
+    end
+  end
+end
+
+bbs_simd = bbs.select { |bb| bb[:has_simd] }
+pp bbs_simd
+puts "SIMD analysis conclude - found #{bbs_simd.count}"
