9ae8f1064a424343ec844a17c20fd6e476299365
Ali Lown <ali@lown.me.uk>
2015-04-24 03:50:34 +0100

    Add test for enlarged offload unit

--- elf/offload_test.vhd
+++ elf/offload_test.vhd
@@ -44,15 +44,16 @@ architecture Behavioral of offload_test is
 
   s_axis_tvalid : in std_logic;
   s_axis_tready : out std_logic;
-  s_axis_tdata : in std_logic_vector(31 downto 0);
-  s_axis_tkeep : in std_logic_vector(3 downto 0);
+  s_axis_tdata : in std_logic_vector(511 downto 0);
+  s_axis_tkeep : in std_logic_vector(63 downto 0);
   s_axis_tlast : in std_logic;
 
   m_axis_tvalid : out std_logic;
   m_axis_tready : in std_logic;
-  m_axis_tdata : out std_logic_vector(31 downto 0);
-  m_axis_tkeep : out std_logic_vector(3 downto 0);
+  m_axis_tdata : out std_logic_vector(511 downto 0);
+  m_axis_tkeep : out std_logic_vector(63 downto 0);
   m_axis_tlast : out std_logic;
+
   dbg : out std_logic_vector(3 downto 0)
 
          );
@@ -63,8 +64,8 @@ architecture Behavioral of offload_test is
   signal rst : std_logic;
 
   signal s_axis_tvalid, s_axis_tready, s_axis_tlast, m_axis_tvalid, m_axis_tready, m_axis_tlast : std_logic;
-  signal s_axis_tdata, m_axis_tdata : std_logic_vector(31 downto 0);
-  signal s_axis_tkeep, m_axis_tkeep : std_logic_vector(3 downto 0);
+  signal s_axis_tdata, m_axis_tdata : std_logic_vector(511 downto 0);
+  signal s_axis_tkeep, m_axis_tkeep : std_logic_vector(63 downto 0);
 
   signal dbg : std_logic_vector(3 downto 0);
 
@@ -108,38 +109,22 @@ begin
     wait for 10ns;
     s_axis_tvalid <= '1';
     s_axis_tlast <= '0';
-    s_axis_tkeep <= "1111";
-    s_axis_tdata <= x"0ffa0ffb";
+    s_axis_tkeep <= x"ffffffffffffffff";
+
+
+    s_axis_tdata <= x"0ffa0ffbeeeeeeeeddddddddccccccccbbbbbbbbaaaaaaaa99999999888888887777777766666666555555554444444433333333222222221111111100000000";
     wait for 10 ns;
-    s_axis_tdata <= x"00000001";
+
+    s_axis_tdata <= x"0ffa0ffb333333332222222233333333222222223333333322222222333333332222222233333333222222223333333322222222333333332222222233333333";
     wait for 10 ns;
-    s_axis_tdata <= x"00000002";
+
+    s_axis_tdata <= x"0ffa0ffbaaaaaaaabbbbbbbbccccccccddddddddaaaaaaaabbbbbbbbccccccccddddddddaaaaaaaabbbbbbbbccccccccddddddddaaaaaaaabbbbbbbbcccccccc";
     wait for 10 ns;
-    s_axis_tdata <= x"00000003";
-    wait for 10 ns;
-    s_axis_tdata <= x"00000004";
-    wait for 10 ns;
-    s_axis_tdata <= x"00000005";
-    wait for 10 ns;
-    s_axis_tdata <= x"00000006";
-    wait for 10 ns;
-    s_axis_tdata <= x"00000007";
-    wait for 10 ns;
-    s_axis_tdata <= x"00000008";
-    wait for 10 ns;
-    s_axis_tdata <= x"00000009";
-    wait for 10 ns;
-    s_axis_tdata <= x"0000000a";
-    wait for 10 ns;
-    s_axis_tdata <= x"0000000b";
-    wait for 10 ns;
-    s_axis_tdata <= x"0000000c";
-    wait for 10 ns;
-    s_axis_tdata <= x"0000000d";
-    wait for 10 ns;
+
     s_axis_tlast <= '1';
-    s_axis_tdata <= x"0000000e";
+    s_axis_tdata <= x"0ffa0ffb111111111111111111111111111111111111111111111111111111111111111122222222222222222222222222222222444444444444444455555555";
     wait for 10 ns;
+
     s_axis_tvalid <= '0';
     s_axis_tlast <= '0';
 
