7df5f385eb08ee89c8f64840352c4cf0bd3b52fa
Ali Lown <ali@lown.me.uk>
2015-08-14 21:28:16 +0100

    Add small loops test case

---
+++ reports/ph3/ex/possibilites/loops.c
@@ -0,0 +1,24 @@
+int unrolled(int a, int b)
+{
+  int sum;
+  for (int i=0; i<10; i++)
+    sum = sum * b + a;
+  return sum;
+}
+
+int rolled(int a, int b, int num)
+{
+  int sum;
+  for (int i=0; i<num; i++)
+    sum = sum * b + a;
+  return sum;
+}
+
+int partial_unrolled(int a, int b, int num)
+{
+  int sum;
+  for (int i=0; i<(num>>2); i++)
+    for (int j=0; j<4; j++)
+      sum = sum * b +a;
+  return sum;
+}
